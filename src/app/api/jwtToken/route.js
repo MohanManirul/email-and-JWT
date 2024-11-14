927f34b7fff8a49c9ca8562512ee2e199adb7d89
MohanManirul <fiforeg@gmail.com>
2024-11-14 12:42:45 +0600

    jwt decoded

---
+++ src/app/api/jwtToken/route.js
@@ -0,0 +1,35 @@
+import { SignJWT, jwtDecrypt, jwtVerify } from "jose";
+import { NextResponse } from "next/server";
+
+// SignJWT , setIssuedAt = property
+ 
+
+// token encode using jwt
+export async function GET() {
+  const secretKey = new TextEncoder().encode(process.env.jWT_SECRET);
+  const payload = {email:"test@gmail.com",user_id:"1"}
+  let token = await new SignJWT(payload) // details to  encode in the token
+    .setProtectedHeader({ alg: "HS256" }) // algorithm
+    .setIssuedAt()
+    .setIssuer(process.env.JWT_ISSUER) // issuer
+    .setExpirationTime(process.env.JWT_EXPIRATION_TIME) // token expiration time, e.g., "1 day"
+    .sign(secretKey); // secretKey generated from previous step
+
+  return NextResponse.json({token:token});
+}
+
+// token decode using jwt
+
+export async function POST(req) {
+  const jsonBody = await req.json();
+  const Token = jsonBody['token'];
+
+  const secretKey = new TextEncoder().encode(process.env.jWT_SECRET);
+
+  try {
+    const  key  = await  jwtVerify(Token, secretKey);
+    return NextResponse.json({ msg: "Token verified", key });
+  } catch (err) {
+    return NextResponse.json({ msg: "Token invalid" });
+  }
+}
--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,35 +1,39 @@
 import { SignJWT, jwtDecrypt, jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 
-// SignJWT , setIssuedAt = property
- 
 
-// token encode using jwt
-export async function GET() {
-  const secretKey = new TextEncoder().encode(process.env.jWT_SECRET);
-  const payload = {email:"test@gmail.com",user_id:"1"}
-  let token = await new SignJWT(payload) // details to  encode in the token
-    .setProtectedHeader({ alg: "HS256" }) // algorithm
-    .setIssuedAt()
-    .setIssuer(process.env.JWT_ISSUER) // issuer
-    .setExpirationTime(process.env.JWT_EXPIRATION_TIME) // token expiration time, e.g., "1 day"
-    .sign(secretKey); // secretKey generated from previous step
 
-  return NextResponse.json({token:token});
-}
+export async function POST(req, res) {
 
-// token decode using jwt
+    const jsonBody = await req.json();
+    let email = jsonBody["email"];
+    let password = jsonBody["password"];
 
-export async function POST(req) {
-  const jsonBody = await req.json();
-  const Token = jsonBody['token'];
 
-  const secretKey = new TextEncoder().encode(process.env.jWT_SECRET);
 
-  try {
-    const  key  = await  jwtVerify(Token, secretKey);
-    return NextResponse.json({ msg: "Token verified", key });
-  } catch (err) {
-    return NextResponse.json({ msg: "Token invalid" });
+    if (email === "test@gmail.com" && password === "123") {
+    
+        const secretKey = new TextEncoder().encode(process.env.jWT_SECRET);
+        const payload = {email:email}
+    
+        let token = await new SignJWT(payload) // details to  encode in the token
+            .setProtectedHeader({ alg: "HS256" }) // algorithm
+            .setIssuedAt()
+            .setIssuer(process.env.JWT_ISSUER) // issuer
+            .setExpirationTime(process.env.JWT_EXPIRATION_TIME) // token expiration time, e.g., "1 day"
+            .sign(secretKey); // secretKey generated from previous step
+    
+      
+      return NextResponse.json(
+        { status: true, message: "Login Success" },
+        { status: 200 }
+      );
+
+    } else {
+      return NextResponse.json( 
+
+        { status: true, message: "Invalid User" },
+        { status: 200});
+    }
+
   }
-}
